0563dbcb3e88bbe24111fccad0209ce8ff8ba374
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-27 14:34:24 +0800

    Generate Vapor project

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "WaceServer",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "WaceServer",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "WaceServerTests",
+            dependencies: [
+                .target(name: "WaceServer"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+] }
